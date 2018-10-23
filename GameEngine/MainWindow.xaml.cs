0cb3665e6672e3703a65eb57e82cf904e87c1dac
Ronald van Engelen <ronvanengelen@gmail.com>
2018-10-23 14:43:44 +0200

    Button test toegvoegd.

--- GameEngine/MainWindow.xaml.cs
+++ GameEngine/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace GameEngine
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // test
+        }
     }
 }
